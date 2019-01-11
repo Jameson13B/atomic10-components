60b6646ce409ab7c1f52e98a7c5f643399aef97c
Jameson13B <jamesonbrown@neolivity.com>
2019-01-11 15:24:48 -0700

    Updates to to do component

--- src/lib/Todo/BWTodo.js
+++ src/lib/Todo/BWTodo.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./BWTodo.css";
+import "../Input/TextInput.css";
+import { TextInput } from "../../lib/index";
 
 class BWTodo extends React.Component {
   constructor(props) {
@@ -11,27 +13,47 @@ class BWTodo extends React.Component {
     };
   }
   componentDidMount() {
-    this.setState({ list: this.props.list, title: this.props.title });
+    if (localStorage.hasOwnProperty("list")) {
+      this.setState({
+        list: JSON.parse(localStorage.getItem("list")),
+        title: this.props.title
+      });
+    } else {
+      this.setState({ list: this.props.list, title: this.props.title });
+      localStorage.setItem("list", JSON.stringify(this.props.list));
+    }
   }
   handleAdd = () => {
     let list = this.state.list;
     list.push(this.state.entry);
     this.setState({ list, entry: "" });
+    localStorage.setItem("list", JSON.stringify(list));
   };
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
+  handleComplete = e => {
+    console.log(e.target.id);
+    let list = this.state.list;
+    list.splice(e.target.id, e.target.id);
+    this.setState({ list });
+    localStorage.setItem("list", JSON.stringify(list));
+  };
+  componentWillUnmount() {
+    localStorage.removeItem("list");
+  }
   render() {
     return (
       <div>
         <div id="myDIV" className="header">
           <h2>{this.state.title}</h2>
-          <input
+          <TextInput
             name="entry"
             onChange={this.handleInputChange}
-            placeholder="Title..."
+            placeholder="New Item..."
             type="text"
             value={this.state.entry}
+            width="100%"
           />
           <span onClick={this.handleAdd} className="addBtn">
             Add
@@ -39,7 +61,14 @@ class BWTodo extends React.Component {
         </div>
         <ul id="myUL">
           {this.state.list.map((li, i) => {
-            return <li id={i}>{li}</li>;
+            return (
+              <li key={i}>
+                <span onClick={this.handleComplete} id={i}>
+                  &otimes;
+                </span>
+                {li}
+              </li>
+            );
           })}
         </ul>
       </div>
@@ -50,7 +79,5 @@ class BWTodo extends React.Component {
 export default BWTodo;
 
 // Attributes
-// value: Button Text
-// click: Callback for onClick
-// bkcolor: Background color, accepts hex, rgb, and string(default: teal)
-// ftcolor: Font color, accepts hex, rgb, and string(default: white)
+// title: To do list title
+// list: An array of todo items
