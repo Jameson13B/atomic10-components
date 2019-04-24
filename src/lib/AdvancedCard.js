2e802f059eeba2d609b7b3b02dae0ab16c9bae9f
Jameson13B <jamesonbrown@neolivity.com>
2019-04-23 21:38:39 -0600

    Remove old css style sheets

--- src/lib/AdvancedCard.js
+++ src/lib/AdvancedCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import './Button.css';
 
 const AdvancedCard = ({
   title,
@@ -36,10 +35,10 @@ const AdvancedCard = ({
 
 export default AdvancedCard;
 
-const SecondaryButton = ({ value, click, color }) => (
+const SecondaryButton = ({ value, onClick, color }) => (
   <CustomButton
     className='a10-secondary-button'
-    onClick={e => click && click(e)}
+    onClick={e => onClick && onClick(e)}
     color={color}
   >
     {value}
@@ -77,5 +76,20 @@ const CustomParagraph = styled.div`
 `;
 const CustomButton = styled.div`
   border: ${props => `2px solid ${props.color || '#FF9933'}`};
+  border-radius: 5px;
   color: ${props => props.color || '#FF9933'};
+  cursor: pointer;
+  display: inline-block;
+  padding: 10px 15px;
+  -webkit-transform: perspective(10px) translateZ(0);
+  transform: perspective(10px) translateZ(0);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+  -webkit-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+  -webkit-transition-property: transform;
+  transition-property: transform;
+  :hover,
+  :active {
+    -webkit-transform: scale(0.9);
+    transform: scale(0.9);
 `;
